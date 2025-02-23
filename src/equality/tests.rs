970731948a7414fe7e131366a0736dfec773d49b
haraldmaida <harald.maida@innoave.com>
2025-02-23 13:17:44 +0100

    use proptest to generate random values for subject in tests

--- src/equality/tests.rs
+++ src/equality/tests.rs
@@ -1,51 +1,161 @@
 use crate::equality::IsEqualTo;
 use crate::specification::assert_that;
+use proptest::prelude::*;
 
-#[test]
-fn string_is_equal_to_string() {
-    let subject: String = "a string result".to_string();
+//
+// String and str
+//
 
-    assert_that(subject).is_equal_to("a string result".to_string());
+proptest! {
+    #[test]
+    fn string_is_equal_to_string(
+        input in any::<String>(),
+    ) {
+        let subject: String = input.clone();
+        let expected: String = input;
+
+        assert_that(subject).is_equal_to(expected);
+    }
+
+    #[test]
+    fn string_is_equal_to_str(
+        input in any::<String>(),
+    ) {
+        let subject: String = input.clone();
+        let expected: &str = &input;
+
+        assert_that(subject).is_equal_to(expected);
+    }
+
+    #[test]
+    fn string_ref_is_equal_to_str(
+        input in any::<String>(),
+    ) {
+        let subject: &String = &input;
+        let expected: &str = &input;
+
+        assert_that::<&String>(subject).is_equal_to(expected);
+    }
+
+    #[test]
+    fn str_is_equal_to_str(
+        input in any::<String>(),
+    ) {
+        let subject: &str = &input;
+        let expected: &str = &input;
+
+        assert_that(subject).is_equal_to(expected);
+    }
+
+    #[test]
+    fn str_is_equal_to_string(
+        input in any::<String>(),
+    ) {
+        let subject: &str = &input.clone();
+        let expected: String = input;
+
+        assert_that(subject).is_equal_to(expected);
+    }
+
+    #[test]
+    fn string_is_not_equal_to_string(
+        (input1, input2) in (any::<String>(), any::<String>()).prop_filter("which are not equal", |(s1, s2)| s1 != s2),
+    ) {
+        let subject: String = input1;
+        let expected: String = input2;
+
+        assert_that(subject).is_not_equal_to(expected);
+    }
+
+    #[test]
+    fn string_is_not_equal_to_str(
+        (input1, input2) in (any::<String>(), any::<String>()).prop_filter("which are not equal", |(s1, s2)| s1 != s2),
+    ) {
+        let subject: String = input1;
+        let expected: &str = &input2;
+
+        assert_that(subject).is_not_equal_to(expected);
+    }
+
+    #[test]
+    fn string_ref_is_not_equal_to_str(
+        (input1, input2) in (any::<String>(), any::<String>()).prop_filter("which are not equal", |(s1, s2)| s1 != s2),
+    ) {
+        let subject: String = input1;
+        let expected: &str = &input2;
+
+        assert_that::<&String>(&subject).is_not_equal_to(expected);
+    }
+
+    #[test]
+    fn str_is_not_equal_to_str(
+        (input1, input2) in (any::<String>(), any::<String>()).prop_filter("which are not equal", |(s1, s2)| s1 != s2),
+    ) {
+        let subject: &str = &input1;
+        let expected: &str = &input2;
+
+        assert_that(subject).is_not_equal_to(expected);
+    }
+
+    #[test]
+    fn str_is_not_equal_to_string(
+        (input1, input2) in (any::<String>(), any::<String>()).prop_filter("which are not equal", |(s1, s2)| s1 != s2),
+    ) {
+        let subject: &str = &input1;
+        let expected: String = input2;
+
+        assert_that(subject).is_not_equal_to(expected);
+    }
 }
 
-#[test]
-fn string_is_equal_to_str() {
-    let subject: String = "a string result".to_string();
+//
+// Integer
+//
 
-    assert_that(subject).is_equal_to("a string result");
+proptest! {
+    #[test]
+    fn usize_is_equal_to_usize(
+        input in any::<usize>(),
+    ) {
+        let subject: usize = input;
+        let expected: usize = input;
+
+        assert_that(subject).is_equal_to(expected);
+    }
+
+    #[test]
+    fn usize_is_not_equal_to_usize(
+        (input1, input2) in (any::<usize>(), any::<usize>()).prop_filter("which are not equal", |(s1, s2)| s1 != s2),
+    ) {
+        let subject: usize = input1;
+        let expected: usize = input2;
+
+        assert_that(subject).is_not_equal_to(expected);
+    }
 }
 
-#[test]
-fn string_ref_is_equal_to_str() {
-    let subject: String = "a string result".to_string();
+//
+// bool
+//
 
-    assert_that::<&String>(&subject).is_equal_to("a string result");
-}
+proptest! {
+    #[test]
+    fn bool_is_equal_to_bool(
+        input in any::<bool>(),
+    ) {
+        let subject: bool = input;
+        let expected: bool = input;
 
-#[test]
-fn str_is_equal_to_str() {
-    let subject: &str = "a string result";
+        assert_that(subject).is_equal_to(expected);
+    }
 
-    assert_that(subject).is_equal_to("a string result");
-}
+    #[test]
+    fn bool_is_not_equal_to_bool(
+        input in any::<bool>(),
+    ) {
+        let subject: bool = input;
+        let expected: bool = !input;
 
-#[test]
-fn str_is_equal_to_string() {
-    let subject: &str = "a string result";
-
-    assert_that(subject).is_equal_to("a string result".to_string());
-}
-
-#[test]
-fn usize_is_equal_to_usize() {
-    let subject: usize = 42;
-
-    assert_that(subject).is_equal_to(42);
-}
-
-#[test]
-fn bool_is_equal_to_bool() {
-    let subject: bool = true;
-
-    assert_that(subject).is_equal_to(true);
+        assert_that(subject).is_not_equal_to(expected);
+    }
 }
