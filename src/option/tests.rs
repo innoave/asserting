e60ca29164614a5632e90dd2a49da9d5cb50048d
haraldmaida <harald.maida@innoave.com>
2025-02-27 04:08:00 +0100

    write tests for `Option` `is_some`/`is_none` and for custom struct

--- src/option/tests.rs
+++ src/option/tests.rs
@@ -1,6 +1,44 @@
 use crate::prelude::*;
 
 #[test]
+fn option_of_i32_is_none() {
+    let subject: Option<i32> = None;
+
+    assert_that(subject).is_none();
+}
+
+#[test]
+fn option_of_i32_is_some() {
+    let subject: Option<i32> = Some(42);
+
+    assert_that(subject).is_some();
+}
+
+#[test]
+fn option_of_string_is_none() {
+    let subject: Option<String> = None;
+
+    assert_that(subject).is_none();
+}
+
+#[test]
+fn option_of_string_is_some() {
+    let subject: Option<String> = Some("te veniam dolore ut".to_string());
+
+    assert_that(subject).is_some();
+}
+
+#[test]
+fn option_of_custom_struct_is_none() {
+    #[derive(Debug)]
+    struct MyStruct;
+
+    let subject: Option<MyStruct> = None;
+
+    assert_that(subject).is_none();
+}
+
+#[test]
 fn option_of_string_has_some_str_value() {
     let subject = Some("non tempor ea delenit".to_string());
 
@@ -19,4 +57,14 @@ fn option_of_str_has_some_string_value() {
     let subject = Some("invidunt commodi eros vel");
 
     assert_that(subject).has_value("invidunt commodi eros vel".to_string());
+}
+
+#[test]
+fn option_of_custom_struct_has_value() {
+    #[derive(Debug, PartialEq)]
+    struct MyStruct;
+
+    let subject = Some(MyStruct);
+
+    assert_that(subject).has_value(MyStruct);
 }
