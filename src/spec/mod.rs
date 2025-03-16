3dadfc5bba61c550a8362b9d6a0a2737e905e4cd
haraldmaida <harald.maida@innoave.com>
2025-03-16 06:41:48 +0100

    write docs for the `assert_that*` and `verify_that*` functions and macros

--- src/spec/mod.rs
+++ src/spec/mod.rs
@@ -11,6 +11,33 @@ use alloc::{
     vec::Vec,
 };
 
+/// Starts an assertion for the given subject or expression in the
+/// [`PanicOnFail`] mode.
+///
+/// It wraps the subject into a [`Spec`] and sets the name of the expression and
+/// the code location of the assertion in the [`Spec`]. On the [`Spec`] any
+/// assertion method that is implemented for the subject's type can be called.
+///
+/// Assertions started with `assert_that!` will panic on the first failing
+/// assertion.
+///
+/// ## Example
+///
+/// ```
+/// use asserting::prelude::*;
+///
+/// assert_that!(7 * 6).is_equal_to(42);
+/// ```
+///
+/// This call of the macro expands to:
+///
+/// ```
+/// # use asserting::prelude::*;
+/// assert_that(7 * 6)
+///     .named("7 * 6")
+///     .located_at(Location { file: file!(), line: line!(), column: column!() })
+///     .is_equal_to(42);
+/// ```
 #[macro_export]
 macro_rules! assert_that {
     ($subject:expr) => {
@@ -24,6 +51,43 @@ macro_rules! assert_that {
     };
 }
 
+/// Starts an assertion for the given subject or expression in the
+/// [`CollectFailures`] mode.
+///
+/// It wraps the subject into a [`Spec`] and sets the name of the expression and
+/// the code location of the assertion in the [`Spec`]. On the [`Spec`] any
+/// assertion method that is implemented for the subject's type can be called.
+///
+/// Assertions started with `verify_that!` will collect [`AssertFailure`]s for
+/// all failing assertions. The collected failures can be queried by calling one
+/// of the methods [`failures`](Spec::failures) or
+/// [`display_failures`](Spec::display_failures) on the [`Spec`].
+///
+/// ## Example
+///
+/// ```
+/// use asserting::prelude::*;
+/// let some_text = "vel tempor augue delenit".to_string();
+/// let failures = verify_that!(some_text)
+///     .starts_with("nibh")
+///     .ends_with("magna")
+///     .failures();
+///
+/// assert_that!(failures).has_length(2);
+/// ```
+///
+/// This call of the macro expands to:
+///
+/// ```
+/// # use asserting::prelude::*;
+/// # let some_text = "vel tempor augue delenit".to_string();
+/// let failures = verify_that(some_text)
+///     .named("some_text")
+///     .located_at(Location { file: file!(), line: line!(), column: column!() })
+///     .starts_with("nibh")
+///     .ends_with("magna")
+///     .failures();
+/// ```
 #[macro_export]
 macro_rules! verify_that {
     ($subject:expr) => {
@@ -37,6 +101,28 @@ macro_rules! verify_that {
     };
 }
 
+/// Starts an assertion for some piece of code in the [`PanicOnFail`] mode.
+///
+/// It takes a closure and wraps it into a [`Spec`]. On the [`Spec`] any
+/// assertion method that is implemented for closures can be called.
+///
+/// Assertions started with `assert_that_code!` will panic on the first failing
+/// assertion.
+///
+/// ## Examples
+///
+/// ```
+/// use asserting::prelude::*;
+///
+/// fn divide(a: i32, b: i32) -> i32 {
+///     a / b
+/// }
+///
+/// assert_that_code!(|| { divide(7, 0); })
+///     .panics_with_message("attempt to divide by zero");
+///
+/// assert_that_code!(|| { divide(7, 3); }).does_not_panic();
+/// ```
 #[cfg(feature = "panic")]
 #[cfg_attr(feature = "panic", macro_export)]
 macro_rules! assert_that_code {
@@ -51,6 +137,52 @@ macro_rules! assert_that_code {
     };
 }
 
+/// Starts an assertion for some piece of code in the [`CollectFailures`] mode.
+///
+/// It takes a closure and wraps it into a [`Spec`]. On the [`Spec`] any
+/// assertion method that is implemented for closures can be called.
+///
+/// Assertions started with `verify_that_code!` will collect [`AssertFailure`]s
+/// for all failing assertions. The collected failures can be queried by calling
+/// one of the methods [`failures`](Spec::failures) or
+/// [`display_failures`](Spec::display_failures) on the [`Spec`].
+///
+/// ## Examples
+///
+/// ```
+/// use asserting::prelude::*;
+///
+/// fn divide(a: i32, b: i32) -> i32 {
+///     a / b
+/// }
+///
+/// let failures = verify_that_code!(|| { divide(7, 3); })
+///     .does_not_panic()
+///     .failures();
+///
+/// assert_that!(failures).is_empty();
+///
+/// let failures = verify_that_code!(|| { divide(7, 0); })
+///     .does_not_panic()
+///     .display_failures();
+///
+/// assert_that!(failures).contains_exactly([
+///     r#"assertion failed: expected || { divide(7, 0); } to not panic, but did panic
+///   with message: "attempt to divide by zero"
+/// "#
+/// ]);
+///
+/// let failures = verify_that_code!(|| { divide(7, 0); })
+///     .panics_with_message("division by zero")
+///     .display_failures();
+///
+/// assert_that!(failures).contains_exactly([
+///     r#"assertion failed: expected || { divide(7, 0); } to panic with message "division by zero"
+///    but was: "attempt to divide by zero"
+///   expected: "division by zero"
+/// "#
+/// ]);
+/// ```
 #[cfg(feature = "panic")]
 #[cfg_attr(feature = "panic", macro_export)]
 macro_rules! verify_that_code {
@@ -65,30 +197,200 @@ macro_rules! verify_that_code {
     };
 }
 
+/// Starts an assertion for the given subject or expression in the
+/// [`PanicOnFail`] mode.
+///
+/// It wraps the subject into a [`Spec`]. On the [`Spec`] any
+/// assertion method that is implemented for the subject's type can be called.
+///
+/// Assertions started with `assert_that()` will panic on the first failing
+/// assertion.
+///
+/// In comparison to using the macro [`assert_that!`](crate::assert_that)
+/// calling this function does not set a name for the expression and does not
+/// set the code location of the assertion. In failure messages the generic word
+/// "subject" is used. To set a specific text for the expression the method
+/// [`named`](Spec::named) must be called explicitly.
+///
+/// Note: It is not necessary to set the code location explicitly as this
+/// function is annotated with `#[track_caller]`.
+///
+/// ## Examples
+///
+/// ```
+/// use asserting::prelude::*;
+///
+/// assert_that(7 * 6).is_equal_to(42);
+/// ```
+///
+/// or with setting a name for the expression:
+///
+/// ```
+/// use asserting::prelude::*;
+///
+/// assert_that(7 * 6)
+///     .named("7 * 6")
+///     .is_equal_to(42);
+/// ```
 #[track_caller]
 pub const fn assert_that<'a, S>(subject: S) -> Spec<'a, S, PanicOnFail> {
     Spec::new(subject, PanicOnFail)
 }
 
+/// Starts an assertion for the given subject or expression in the
+/// [`CollectFailures`] mode.
+///
+/// It wraps the subject into a [`Spec`]. On the [`Spec`] any
+/// assertion method that is implemented for the subject's type can be called.
+///
+/// Assertions started with `verify_that()` will collect [`AssertFailure`]s
+/// for all failing assertions. The collected failures can be queried by calling
+/// one of the methods [`failures`](Spec::failures) or the
+/// [`display_failures`](Spec::display_failures) on the [`Spec`].
+///
+/// In comparison to using the macro [`verify_that!`](crate::verify_that) calling
+/// this function does not set a name for the expression and does not set the
+/// code location of the assertion. In failure messages the generic word
+/// "subject" is used. To set a specific text for the expression the method
+/// [`named`](Spec::named) must be called explicitly.
+///
+/// ## Examples
+///
+/// ```
+/// use asserting::prelude::*;
+///
+/// let some_text = "vel tempor augue delenit".to_string();
+///
+/// let failures = verify_that(some_text).named("my_thing")
+///     .starts_with("nibh")
+///     .ends_with("magna")
+///     .failures();
+///
+/// assert_that!(failures).has_length(2);
+/// ```
+///
+/// or with querying the failures as formated text:
+///
+/// ```
+/// use asserting::prelude::*;
+///
+/// let some_text = "vel tempor augue delenit".to_string();
+///
+/// let failures = verify_that(some_text).named("my_thing")
+///     .starts_with("nibh")
+///     .ends_with("magna")
+///     .display_failures();
+///
+/// assert_that!(failures).contains_exactly([
+///     r#"assertion failed: expected my_thing to start with "nibh"
+///    but was: "vel tempor augue delenit"
+///   expected: "nibh"
+/// "#,
+///     r#"assertion failed: expected my_thing to end with "magna"
+///    but was: "vel tempor augue delenit"
+///   expected: "magna"
+/// "#,
+/// ]);
+/// ```
 #[track_caller]
 pub const fn verify_that<'a, S>(subject: S) -> Spec<'a, S, CollectFailures> {
     Spec::new(subject, CollectFailures)
 }
 
+/// Starts an assertion for some piece of code in the [`PanicOnFail`] mode.
+///
+/// It takes a closure and wraps it into a [`Spec`]. On the [`Spec`] any
+/// assertion method that is implemented for closures can be called.
+///
+/// Assertions started with `assert_that_code()` will panic on the first failing
+/// assertion.
+///
+/// In comparison to using the macro [`assert_that_code!`](crate::assert_that_code)
+/// calling this function does not set a name for the expression and does not
+/// set the code location of the assertion. In failure messages the generic word
+/// "the closure" is used. To set a specific text for the expression the method
+/// [`named`](Spec::named) must be called explicitly.
+///
+/// ## Examples
+///
+/// ```
+/// use asserting::prelude::*;
+///
+/// fn divide(a: i32, b: i32) -> i32 {
+///     a / b
+/// }
+///
+/// assert_that_code(|| { divide(7, 0); })
+///     .panics_with_message("attempt to divide by zero");
+///
+/// assert_that_code(|| { divide(7, 3); }).does_not_panic();
+/// ```
 #[cfg(feature = "panic")]
 pub fn assert_that_code<'a, S>(code: S) -> Spec<'a, Code<S>, PanicOnFail>
 where
     S: FnOnce(),
 {
-    Spec::new(Code::from(code), PanicOnFail)
+    Spec::new(Code::from(code), PanicOnFail).named("the closure")
 }
 
+/// Starts an assertion for some piece of code in the [`CollectFailures`] mode.
+///
+/// It takes a closure and wraps it into a [`Spec`]. On the [`Spec`] any
+/// assertion method that is implemented for closures can be called.
+///
+/// Assertions started with `verify_that_code()` will collect [`AssertFailure`]s
+/// for all failing assertions. The collected failures can be queried by calling
+/// one of the methods [`failures`](Spec::failures) or
+/// [`display_failures`](Spec::display_failures) on the [`Spec`].
+///
+/// In comparison to using the macro [`verify_that_code!`](crate::verify_that_code)
+/// calling this function does not set a name for the expression and does not
+/// set the code location of the assertion. In failure messages the generic word
+/// "the closure" is used. To set a specific text for the expression the method
+/// [`named`](Spec::named) must be called explicitly.
+///
+/// ## Examples
+///
+/// ```
+/// use asserting::prelude::*;
+///
+/// fn divide(a: i32, b: i32) -> i32 {
+///     a / b
+/// }
+///
+/// let failures = verify_that_code(|| { divide(7, 3); })
+///     .does_not_panic()
+///     .failures();
+///
+/// assert_that!(failures).is_empty();
+///
+/// let failures = verify_that_code(|| { divide(7, 0); })
+///     .does_not_panic()
+///     .display_failures();
+///
+/// assert_that!(failures).contains_exactly([
+///     r#"assertion failed: expected the closure to not panic, but did panic
+///   with message: "attempt to divide by zero"
+/// "#
+/// ]);
+///
+/// let failures = verify_that_code(|| { divide(7, 0); })
+///     .panics_with_message("division by zero")
+///     .display_failures();
+///
+/// assert_that!(failures).contains_exactly([
+///     r#"assertion failed: expected the closure to panic with message "division by zero"
+///    but was: "attempt to divide by zero"
+///   expected: "division by zero"
+/// "#
+/// ]);
+/// ```
 #[cfg(feature = "panic")]
 pub fn verify_that_code<'a, S>(code: S) -> Spec<'a, Code<S>, CollectFailures>
 where
     S: FnOnce(),
 {
-    Spec::new(Code::from(code), CollectFailures)
+    Spec::new(Code::from(code), CollectFailures).named("the closure")
 }
 
 pub trait Expectation<S: ?Sized> {
