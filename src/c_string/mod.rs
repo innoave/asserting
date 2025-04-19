c3dceffdf82e3cfcdce7fe123fdd766d8c46e72f
haraldmaida <harald.maida@innoave.com>
2025-04-19 04:42:46 +0200

    feat: implement `LengthProperty` for `CString` and `CStr`

--- src/c_string/mod.rs
+++ src/c_string/mod.rs
@@ -1,6 +1,6 @@
 //! Implementation of assertions for `CString` and `CStr` values.
 
-use crate::properties::IsEmptyProperty;
+use crate::properties::{IsEmptyProperty, LengthProperty};
 use crate::std::ffi::{CStr, CString};
 
 impl IsEmptyProperty for CString {
@@ -12,6 +12,18 @@ impl IsEmptyProperty for CString {
 impl IsEmptyProperty for &CStr {
     fn is_empty_property(&self) -> bool {
         self.is_empty()
+    }
+}
+
+impl LengthProperty for CString {
+    fn length_property(&self) -> usize {
+        self.as_bytes().len()
+    }
+}
+
+impl LengthProperty for &CStr {
+    fn length_property(&self) -> usize {
+        self.to_bytes().len()
     }
 }
 
