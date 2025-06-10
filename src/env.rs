f6915ec826feeddf86ec2b6b0ba7a6155880e2d5
haraldmaida <harald.maida@innoave.com>
2025-06-10 05:39:33 +0200

    fix: fake environment should not take over project relevant env variables from real environment

--- src/env.rs
+++ src/env.rs
@@ -19,7 +19,12 @@ mod fake_env {
     use std::env::VarError;
 
     thread_local! {
-        static ENV_STORE: RefCell<EnvStore> = RefCell::new(EnvStore::fake());
+        static ENV_STORE: RefCell<EnvStore> = RefCell::new({
+            let env = EnvStore::fake();
+            env.remove_var("ASSERTING_HIGHLIGHT_DIFFS");
+            env.remove_var("NO_COLOR");
+            env
+        });
     }
 
     pub fn var(key: &str) -> Result<String, VarError> {
